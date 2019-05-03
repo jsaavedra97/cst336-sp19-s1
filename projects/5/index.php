9b174c04ee7de9985f621abb761bd694a0297d55
Joshua Saavedra <jsaavedra@csumb.edu>
2019-05-03 20:16:35 +0000

    project 5 and misc

---
+++ projects/5/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Project 5</title>
+        <meta charset="utf-8" />
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    
+    <body>
+        <div>Hello World!</div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="js/functions.js"></script>
+    </body>
+</html>
